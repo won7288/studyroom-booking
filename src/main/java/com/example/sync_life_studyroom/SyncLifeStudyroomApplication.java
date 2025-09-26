45ebfc204438ab9471924b5a2396d43d91c8f038
wonjong <won72881@naver.com>
2025-09-26 20:54:39 +0900

    9/26 - 초기커밋

---
+++ src/main/java/com/example/sync_life_studyroom/SyncLifeStudyroomApplication.java
@@ -0,0 +1,13 @@
+package com.example.sync_life_studyroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyncLifeStudyroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyncLifeStudyroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sync_life_studyroom/SyncLifeStudyroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sync_life_studyroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncLifeStudyroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
